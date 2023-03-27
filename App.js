b223af74a23b414867cef8f5dd49c5f2e9640e9d
Dylan Wong <dylanwong@nevada.unr.edu>
2023-03-27 10:04:18 -0700

    string diff

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as TextUtils from './TextUtils';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button onPress={() => console.log(TextUtils.default('おねがいします。', 'おねがいじます。'))} title="press me" />
       <StatusBar style="auto" />
     </View>
   );
